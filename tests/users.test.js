60b7e0366233bbd014d2c9e0ceaba99f5abc8b4e
David Duranty <dadaduranty@gmail.com>
2023-11-28 15:22:51 +0100

    add solutions quetes 2.1

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const crypto = require("node:crypto");
 
 const app = require("../src/app");
 
@@ -25,6 +26,53 @@ describe("GET /api/users/:id", () => {
     const response = await request(app).get("/api/users/0");
 
     expect(response.status).toEqual(404);
+  });
+});
+describe("POST /api/users", () => {
+  it("should return created user", async () => {
+    const newUser = {
+      firstname: "Dimitriov",
+      lastname: "Dimitries",
+      email: `${crypto.randomUUID()}@wild.co`,
+      city: "Mexique",
+      language: "Chelou",
+    };
+
+    const response = await request(app).post("/api/users").send(newUser);
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM users WHERE id=?",
+      response.body.id
+    );
+
+    const [movieInDatabase] = result;
+
+    expect(movieInDatabase).toHaveProperty("id");
+
+    expect(movieInDatabase).toHaveProperty("firstname");
+    expect(movieInDatabase.firstname).toStrictEqual(newUser.firstname);
+    expect(typeof movieInDatabase.firstname).toBe("string");
+    expect(movieInDatabase).toHaveProperty("lastname");
+    expect(typeof movieInDatabase.lastname).toBe("string");
+    expect(movieInDatabase).toHaveProperty("city");
+    expect(typeof movieInDatabase.city).toBe("string");
+    expect(movieInDatabase).toHaveProperty("language");
+    expect(typeof movieInDatabase.language).toBe("string");
+  });
+
+  it("should return an error", async () => {
+    const movieWithMissingProps = { firstname: "Axel" };
+
+    const response = await request(app)
+      .post("/api/users")
+      .send(movieWithMissingProps);
+
+    expect(response.status).toEqual(500);
   });
 });
 
